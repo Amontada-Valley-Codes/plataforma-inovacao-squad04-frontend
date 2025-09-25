b514dccd14819fa4a71f5330e20fe2cc26946efe
Iago <motaiago443@gmail.com>
2025-09-25 18:52:18 -0300

    feat: LoginPage

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginPage from "@/components/LoginPage/LoginPage";
+
+export default function Login() {
+  return (
+    <div className="w-full h-full">
+      <LoginPage/>
+    </div>
+  )
+}
---
+++ src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import AuthVisual from "../AuthVisual";
+import LoginForm from "./LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="w-full h-screen grid grid-cols-[1fr_640px]">
+      {/* Coluna esquerda */}
+      <AuthVisual />
+        <LoginForm />
+    </div>
+  );
+}
