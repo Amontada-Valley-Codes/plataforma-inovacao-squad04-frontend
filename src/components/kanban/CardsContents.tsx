98e174bd09bfe18de0505185485ad060793beb6d
GabrielXavier77 <gabrielxavier270723@gmail.com>
2025-10-06 21:55:27 -0300

    card expanded pre-screening

--- src/components/kanban/CardsContents.tsx
+++ src/components/kanban/CardsContents.tsx
@@ -1,5 +1,7 @@
 import { BriefcaseBusiness, Building2, Lightbulb, Menu } from "lucide-react";
 import ForwardButton from "./ForwardButton"
+import { useState } from 'react';
+import { HelpCircle } from 'lucide-react';
 
 type CardChallangeContentProps = {
   challangeTitle: string;
@@ -12,7 +14,7 @@ type CardChallangeContentProps = {
 export const CardChallangeContent = ({ challangeTitle, category, description, featureId, handleApproveAndMove}: CardChallangeContentProps) => {
  return (
   //conteudo do card de desafio
-  <div className="flex flex-col rounded-bl-2xl">
+  <div className="flex flex-col rounded-bl-2xl overflow-y-auto scrollbar-hidden">
     {/* header */}
     <div className="mb-6">
       <h1 className="text-[28px] text-[#0B2B70] font-semibold">{challangeTitle}</h1>
@@ -80,16 +82,120 @@ export const CardChallangeContent = ({ challangeTitle, category, description, fe
  )
 }
 
+export const Rating = ({ initialScore = 0 }) => {
+  const [score, setScore] = useState(initialScore);
+  const ratingOptions = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="mt-3 flex items-center ml-2 gap-3">
+      {ratingOptions.map((num) => (
+        <div className="flex flex-col justify-center items-center gap-[2px]">
+          <p className="text-[10px]">{num}</p>
+          <button
+            key={num}
+            onClick={() => setScore(num)}
+            className={`h-5 w-5 rounded-full flex items-center justify-center 
+            font-semibold text-sm transition-all duration-200 bg-[#D9D9D9] focus:outline-none 
+            focus:ring-2 focus:ring-blue-400 focus:ring-offset-2`}
+          >
+            <div className={`h-3 w-3 rounded-full ${
+              num === score ? "bg-[#0B2B72]" : ""
+            }`}>
+
+            </div>
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const ProgressBarActions = ({percentage}: {percentage: number}) => {
+  return (
+    <div className="mb-6">
+      <div>
+        <p className="text-green-600 font-bold text-sm">
+          {percentage}% <span className="text-[#666]">Aprovado</span>
+        </p>
+        <div className="mt-1 w-full bg-gray-200 rounded-full h-2">
+          <div
+            className="bg-green-500 h-2 rounded-full"
+            style={{ width: `${percentage}%` }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 type CardPreScreeningContentProps = {
+  challangeTitle: string;
+  category: string;
+  description: string;
   featureId: string | undefined;
   handleApproveAndMove: (featureId: string | undefined) => void;
 }
 
-export const CardPreScreeningContent = ({ featureId, handleApproveAndMove }: CardPreScreeningContentProps) => {
+export const CardPreScreeningContent = ({ challangeTitle, category, description, featureId, handleApproveAndMove}: CardChallangeContentProps) => {
  return (
-  <div>
-    <p>Desafio</p>
-    <ForwardButton featureId={featureId} handleApproveAndMove={handleApproveAndMove}/>
+  <div className="flex flex-col rounded-bl-2xl overflow-y-auto scrollbar-hidden">
+    {/* header */}
+    <div className="mb-6">
+      <h1 className="text-[28px] text-[#0B2B70] font-semibold">{challangeTitle}</h1>
+      <button className="bg-[#0B2B70] text-[10px] text-white font-semibold w-fit rounded-[8px] px-4 py-1">
+        {category.toUpperCase()}
+      </button>
+    </div>
+
+    {/* conteudo */}
+    <div>
+      {/* alinhamento estrategico */}
+      <div className="flex flex-col mb-6">
+        <h1 className="flex gap-1 items-center text-black text-lg">
+          <Building2 size={16}/>
+          Alinhamento Estratégico
+        </h1>
+        <p className="text-sm text-gray-600 font-medium">
+          {description}
+        </p>
+        <Rating/>
+      </div>
+
+      {/* potencial inovador */}
+      <div className="flex flex-col mb-6">
+        <h1 className="flex gap-1 items-center text-black text-lg">
+          <Lightbulb size={16}/>
+          Potência Inovador
+        </h1>
+        <p className="text-sm text-gray-600 font-medium">
+          {description}
+        </p>
+        <Rating/>
+      </div>
+
+      {/* relevancia do negocio */}
+      <div className="flex flex-col mb-6">
+        <h1 className="flex gap-1 items-center text-black text-lg">
+          <BriefcaseBusiness size={16}/>
+          Relevância para o negócio
+        </h1>
+        <p className="text-sm text-gray-600 font-medium">
+          {description}
+        </p>
+        <Rating/>
+      </div>
+    </div>
+
+    <ProgressBarActions percentage={66}/>
+
+    <div className="w-full flex justify-center gap-6">
+      <button className="flex w-45 justify-center px-1 py-2 
+      rounded-[8px] bg-[#E7EEFF] hover:bg-[#dee2ec] transition-colors text-[#0B2B70] font-semibold
+      text-[12px] cursor-pointer">
+        Voltar
+      </button>
+      <ForwardButton className="w-45" featureId={featureId} handleApproveAndMove={handleApproveAndMove}/>
+    </div>
   </div>
  )
 }
