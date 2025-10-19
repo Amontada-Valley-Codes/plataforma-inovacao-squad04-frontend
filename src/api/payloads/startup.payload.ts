9365163de3d5db273510ecbddeeade6c3c69ff6a
Eduardo Albuquerque <eduardoalbuquerque3021@gmail.com>
2025-10-19 16:15:25 -0300

    feat: crate startup crud

--- src/api/payloads/startup.payload.ts
+++ src/api/payloads/startup.payload.ts
@@ -1,18 +1,53 @@
 export type ShowAllStartupsResponse = {
   logo?: string;
-  id: string
-  name: string
-  cnpj: string
-  industry_segment: string
-  problems_solved: string[]
-  technologies_used: string[]
-  maturity_stage: string
-  location: string
-  founders: string[]
-  pitch: string
+  id: string;
+  name: string;
+  cnpj: string;
+  industry_segment: string;
+  problems_solved: string[];
+  technologies_used: string[];
+  maturity_stage: string;
+  location: string;
+  founders: string[];
+  pitch: string;
   useful_links: {
-    github: string
-    website: string
-    linkedin: string
+    github: string;
+    website: string;
+    linkedin: string;
+  }
+}
+
+export type CreateStartupPayload = {
+  name: string;
+  cnpj: string;
+  industry_segment: string;
+  problems_solved: string[];
+  technologies_used: string[];
+  maturity_stage: string;
+  location: string;
+  founders: string[];
+  pitch: string;
+  useful_links: {
+    github: string;
+    website: string;
+    linkedin: string;
+  };
+}
+
+export type CreateStartupResponse = {
+  id: string;
+  name: string;
+  cnpj: string;
+  industry_segment: string;
+  problems_solved: string[];
+  technologies_used: string[];
+  maturity_stage: string;
+  location: string;
+  founders: string[];
+  pitch: string;
+  useful_links: {
+    github: string;
+    website: string;
+    linkedin: string;
   }
 }
--- src/api/services/startup.service.ts
+++ src/api/services/startup.service.ts
@@ -1,10 +1,16 @@
 import api from "../axios";
 import { ENDPOINTS } from "../endpoints";
-import { ShowAllStartupsResponse } from "../payloads/startup.payload";
+import { CreateStartupPayload, CreateStartupResponse, ShowAllStartupsResponse } from "../payloads/startup.payload";
 
 export const startupService = {
   async showAllStartups(): Promise<ShowAllStartupsResponse[]> {
     const response = await api.get(ENDPOINTS.STARTUP.SHOW_ALL);
+    console.log(response.data);
+    return response.data;
+  },
+
+  async createStartup(createStartupPayload: CreateStartupPayload): Promise<CreateStartupResponse> {
+    const response = await api.post(ENDPOINTS.STARTUP.CREATE, createStartupPayload);
     console.log(response.data);
     return response.data;
   }
