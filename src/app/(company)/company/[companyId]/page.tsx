3fd304d4cd95446a9b6fbc160ce353d8c1e3bcb2
Eduardo Albuquerque <eduardoalbuquerque3021@gmail.com>
2025-10-23 22:38:13 -0300

    ci: vercel

--- src/app/(company)/company/[companyId]/page.tsx
+++ src/app/(company)/company/[companyId]/page.tsx
@@ -1,22 +1,14 @@
-import { FC } from "react";
+export default function Page({ params }: { params: { companyId: string } }) {
+  const { companyId } = params;
 
-type PageProps = {
-  params: {
-    companyId: string;
-  };
-};
-
-const Page: FC<PageProps> = ({ params }) => {
   return (
     <div className="p-4 sm:p-6 md:p-8 w-full max-w-screen-lg mx-auto overflow-x-hidden">
       <h1 className="text-lg sm:text-xl md:text-2xl font-semibold mb-2">
         Company Dashboard
       </h1>
       <p className="text-sm sm:text-base text-gray-700 dark:text-gray-300 break-all">
-        companyId: <b>{params.companyId}</b>
+        companyId: <b>{companyId}</b>
       </p>
     </div>
   );
-};
-
-export default Page;
+}
