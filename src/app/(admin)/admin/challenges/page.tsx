164e22b609fc20e11190556bf187a5ea1cbfb22c
DevLaraBarros <laragabryella7@gmail.com>
2025-10-25 09:42:55 -0300

    chore(ui): remove Suspense redundantes e simplifica carregamento de componentes

--- src/app/(admin)/admin/challenges/page.tsx
+++ src/app/(admin)/admin/challenges/page.tsx
@@ -1,12 +1,9 @@
 import ChallengeCard from "@/components/challenge/ChallengeCard";
-import { Suspense } from "react";
 
 export default function challenges() {
   return(
     <div>
-      <Suspense fallback={<p>Carregando...</p>}>
         <ChallengeCard />
-      </Suspense>
     </div>
   );
 }
