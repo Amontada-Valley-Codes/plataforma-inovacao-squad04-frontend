f0c41cdfc8c117de7d3c9e4d392872ade22c2391
Eduardo Albuquerque <eduardoalbuquerque3021@gmail.com>
2025-10-24 22:11:32 -0300

    ci: deployment to vercel

--- src/components/RegisterPage/RegisterPage.tsx
+++ src/components/RegisterPage/RegisterPage.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
 import AuthVisual from "../AuthVisual";
 import LoginForm from "./RegisterForm";
 
 export default function RegisterPage() {
   return (
     <div className="w-full h-screen grid grid-cols-[1fr_640px]">
+      
       <AuthVisual />
-        <LoginForm />
+        <Suspense fallback={<div>Carregando...</div>}>
+          <LoginForm />
+        </Suspense>
     </div>
   );
 }
