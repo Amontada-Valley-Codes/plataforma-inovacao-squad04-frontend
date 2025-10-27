df3b81f10843f80445d80989df7edb58542766ae
DevLaraBarros <laragabryella7@gmail.com>
2025-10-27 14:44:39 -0300

    feat(historico): admin - vê todos os arquivados e concluidos, gestor - vê apenas o da sua empresa, usuario - vê apenas o que ele fez

--- src/api/services/challenge.service.ts
+++ src/api/services/challenge.service.ts
@@ -1,41 +1,57 @@
+// src/api/services/challenge.service.ts
 import api from "../axios";
 import { ENDPOINTS } from "../endpoints";
-import { ChangeStatusPayload, ChangeStatusResponse, ChangeVisibilityPayload, ChangeVisibilityResponse, CreateChallengePayload, CreateChallengeResponse, ShowAllChallengeResponse, ShowAllPubliChallengeResponse, ShowOneChallengeResponse } from "../payloads/challenge.payload";
+import {
+  ChangeStatusPayload,
+  ChangeStatusResponse,
+  ChangeVisibilityPayload,
+  ChangeVisibilityResponse,
+  CreateChallengePayload,
+  CreateChallengeResponse,
+  ShowAllChallengeResponse,
+  ShowAllPubliChallengeResponse,
+  ShowOneChallengeResponse,
+} from "../payloads/challenge.payload";
 
 export const ChallengeService = {
   async createChallenge(createChallengePayload: CreateChallengePayload): Promise<CreateChallengeResponse> {
     const response = await api.post(ENDPOINTS.CHALLENGE.CREATE, createChallengePayload);
-    console.log(response.data);
     return response.data;
   },
 
   async showAllChallenges(): Promise<ShowAllChallengeResponse[]> {
     const response = await api.get(ENDPOINTS.CHALLENGE.SHOW_ENTERPRISE_CHALLENGE);
-    console.log(response.data);
     return response.data;
   },
 
   async showOneChallenge(id: string): Promise<ShowOneChallengeResponse> {
     const response = await api.get(ENDPOINTS.CHALLENGE.SHOW_ONE_CHALLENGE(id));
-    console.log(response.data);
     return response.data;
   },
 
   async changeStatus(id: string, changeStatusPayload: ChangeStatusPayload): Promise<ChangeStatusResponse> {
     const response = await api.patch(ENDPOINTS.CHALLENGE.UPDATE_STATUS(id), changeStatusPayload);
-    console.log(response.data);
-    return response.data
+    return response.data;
   },
 
   async changeVisibility(id: string, changeVisibilityPayload: ChangeVisibilityPayload): Promise<ChangeVisibilityResponse> {
     const response = await api.patch(ENDPOINTS.CHALLENGE.UPDATE_VISIBILITY(id), changeVisibilityPayload);
-    console.log(response.data);
-    return response.data
+    return response.data;
   },
 
   async showAllPublicChallenges(): Promise<ShowAllPubliChallengeResponse> {
     const response = await api.get(ENDPOINTS.CHALLENGE.SHOW_ALL_PUBLIC_CHALLENGE);
-    console.log(response.data);
     return response.data;
   },
-}
+
+  async showHistorical(params?: {
+    enterpriseId?: string;
+    createdById?: string;
+    status?: "Completed" | "Archived";
+  }): Promise<ShowAllChallengeResponse[]> {
+    const response = await api.get(ENDPOINTS.CHALLENGE.SHOW_CHALLENGE_HISTORICAL_ENTERPRISE, {
+      params,
+    });
+    return response.data;
+  },
+};
