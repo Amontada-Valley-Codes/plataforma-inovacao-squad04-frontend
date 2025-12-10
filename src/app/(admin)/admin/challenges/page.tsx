743fca4f12c088417a6935e4218ebcd1dc3846c0
Eduardo Albuquerque <eduardoalbuquerque3021@gmail.com>
2025-12-09 21:48:09 -0300

    chore: add turbopack config

--- src/app/(admin)/admin/challenges/page.tsx
+++ src/app/(admin)/admin/challenges/page.tsx
@@ -1,7 +1,7 @@
 import ChallengeCard from "@/components/challenge/ChallengeCard";
 import { Suspense } from "react";
 
-export default function challenges() {
+export default function Challenges() {
   return(
     <Suspense fallback={<p>carregando...</p>}>
         <ChallengeCard />
