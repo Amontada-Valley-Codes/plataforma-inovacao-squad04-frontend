b76409df05538d28718ad254f2b92410e3392667
Eduardo Albuquerque <eduardoalbuquerque3021@gmail.com>
2026-01-07 15:14:04 -0300

    config

--- src/app/(admin)/admin/challenges/page.tsx
+++ src/app/(admin)/admin/challenges/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import ChallengeCard from "@/components/challenge/ChallengeCard";
+import { Suspense } from "react";
 
 export default function Challenges() {
   return(
-    <ChallengeCard />
+    <Suspense>
+      <ChallengeCard />
+    </Suspense>
   );
 }
