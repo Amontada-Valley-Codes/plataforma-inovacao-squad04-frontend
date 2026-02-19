77516281a8df06ea16eee4a24bfa4bbf582202cc
Eduardo Albuquerque <eduardoalbuquerque3021@gmail.com>
2026-02-18 23:51:35 -0300

    test

--- src/components/challenge/ApplyButton.tsx
+++ src/components/challenge/ApplyButton.tsx
@@ -76,7 +76,7 @@ export default function ApplyButton({
     >
       {loading ? (
         <>
-          <Loader2 className="h-4 w-4 animate-spin" aria-hidden />
+          <Loader2 className="h-4 w-4 animate-spin" aria-hidden/>
           Processando…
         </>
       ) : (
